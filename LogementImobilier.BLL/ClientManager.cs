42040bc1cf94e6d6254567c8db26b0da2cefdf70
arolleaguekeng <93347411+arolleaguekeng@users.noreply.github.com>
2021-11-20 14:02:00 -0800

    Revert "configuration du bouton login et cofiguration du formulaire add user"

--- LogementImobilier.BLL/ClientManager.cs
+++ LogementImobilier.BLL/ClientManager.cs
@@ -1,6 +1,4 @@
-﻿using LogementImobilier.BO;
-using LogementImobilier.DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +6,8 @@ using System.Threading.Tasks;
 
 namespace LogementImobilier.BLL
 {
-    
-    public class ClientManager
+    class ClientManager
     {
-        public ClientRepository clientRepository;
-
-        public ClientManager()
-        {
-            clientRepository = new ClientRepository();
-        }
-        public void AddClient(Client client)
-        {
-            clientRepository.Add(client);
-        }
         //ksdfjhsdjkfsdhfkj 
     }
 }
